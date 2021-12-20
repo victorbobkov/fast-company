6124d40ce2adf20ee9f0c8d7f19bed1c7d2125c8
victor bobkov <victorbobkov@gmail.com>
2021-12-20 23:08:12 +0300

    handling registration errors

--- src/app/components/ui/registerForm.jsx
+++ src/app/components/ui/registerForm.jsx
@@ -88,7 +88,7 @@ const RegisterForm = () => {
 
    const isValid = Object.keys(errors).length === 0
 
-   const handleSubmit = (e) => {
+   const handleSubmit = async (e) => {
       e.preventDefault()
       const isValid = validate()
       if (!isValid) return
@@ -96,8 +96,12 @@ const RegisterForm = () => {
          ...data,
          qualities: data.qualities.map(q => q.value
          )}
-      console.log(newData)
-      signUp(newData)
+      
+      try {
+         await signUp(newData)
+      } catch (error) {
+         setErrors(error)
+      }
    }
 
    return (
--- src/app/hooks/useAuth.jsx
+++ src/app/hooks/useAuth.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import axios from 'axios'
 import userService from '../services/user.service'
 import {toast} from 'react-toastify'
+import {setTokens} from '../services/localStorage.service'
 
 const httpAuth = axios.create()
 const AuthContext = React.createContext()
@@ -11,24 +12,12 @@ export const useAuth = () => {
    return useContext(AuthContext)
 }
 
-const TOKEN_KEY = 'jwt-token'
-const REFRESH_KEY = 'jwt-refresh-token'
-const EXPIRES_KEY = 'jwt-expires'
-
 const AuthProvider = ({children}) => {
    const [currentUser, setUser] = useState({})
    const [error, setError] = useState(null)
 
-   function setTokens({refreshToken, idToken, expiresIn = 3600}) {
-      const expiresDate = new Date().getTime() + expiresIn * 1000
-      localStorage.setItem(TOKEN_KEY, idToken)
-      localStorage.setItem(REFRESH_KEY, refreshToken)
-      localStorage.setItem(EXPIRES_KEY, expiresDate)
-   }
-
    async function signUp({email, password, ...rest}) {
       const url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${process.env.REACT_APP_FIREBASE_KEY}`
-
       try {
          const {data} = await httpAuth.post(url, {
             email,
@@ -36,12 +25,24 @@ const AuthProvider = ({children}) => {
             returnSecureToken: true
          })
          setTokens(data)
+
          await createUser({_id: data.localId, email, ...rest})
          console.log(data)
       } catch (error) {
          errorCatcher(error)
+         const {code, message} = error.response.data.error
+         console.log(code, message)
+         if (code === 400) {
+            if (message === 'EMAIL_EXISTS') {
+               const errorObject = {
+                  email: "Пользователь с таким Email уже существует"
+               }
+               throw errorObject
+            }
+         }
       }
    }
+
    async function createUser(data) {
       try {
          const {content} = await userService.create(data)
