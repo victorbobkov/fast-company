e0c8bb1cff885bfe26d4cdd122b961d762209c71
Victor Bobkov <victorbobkov@gmail.com>
2022-04-06 20:45:12 +0400

    Add SignUp logic

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,9 +1,47 @@
 const express = require('express')
+const bcrypt = require('bcryptjs')
+const User = require('../models/User')
+const { generateUserData } = require("../utils/helpers")
+const tokenService = require('../services/token.service')
 const router = express.Router({ mergeParams: true })
 
 // /api/auth/signUp
+// 1. get data from req (email, password, etc)
+// 2. check if user already exists
+// 3. hash password
+// 4. create user
+// 5. generate jwt-tokens
 router.post('/signUp', async (req, res) => {
+    try {
+        const { email, password } = req.body
 
+        const existingUser = await User.findOne({ email: email })
+        if (existingUser) {
+            return res.status(400).json({
+                error: {
+                    message: 'EMAIL_EXISTS',
+                    code: 400
+                }
+            })
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 12)
+
+        const newUser = await User.create({
+            ...generateUserData(),
+            ...req.body,
+            password: hashedPassword,
+        })
+
+        const tokens = tokenService.generate({ _id: newUser._id })
+
+        res.status(201).send({ ...tokens, userId: newUser._id })
+
+    } catch (e) {
+        res.status(500).json({
+            message: 'Internal Server Error. Try again later'
+        })
+    }
 })
 
 router.post('/signInWithPassword', async (req, res) => {
