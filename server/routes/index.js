76bdbf1b443fee836df70660a68e99bb40d8a8cb
Victor Bobkov <victorbobkov@gmail.com>
2022-04-06 19:24:33 +0400

    Get professions and qualities

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,9 +3,9 @@ const router = express.Router({ mergeParams: true })
 
 // /api/auth
 router.use('/auth', require('./auth.routes'))
-router.use('/comment', require('./auth.routes'))
-router.use('/quality', require('./auth.routes'))
-router.use('/profession', require('./auth.routes'))
-router.use('/user', require('./auth.routes'))
+router.use('/comment', require('./comment.routes'))
+router.use('/quality', require('./quality.routes'))
+router.use('/profession', require('./profession.routes'))
+router.use('/user', require('./user.routes'))
 
 module.exports = router
--- server/routes/quality.routes.js
+++ server/routes/quality.routes.js
@@ -1,4 +1,16 @@
 const express = require('express')
+const Quality = require('../models/Qualitiy')
 const router = express.Router({ mergeParams: true })
+
+router.get('/', async (req, res) => {
+    try {
+        const qualities = await Quality.find()
+        res.status(200).send(qualities)
+    } catch (e) {
+        res.status(500).json({
+            message: 'Internal Server Error. Try again later'
+        })
+    }
+})
 
 module.exports = router
